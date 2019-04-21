5262801682ea7ea8a6f0dd2668c4433b151b0991
church <simonreuben23@gmail.com>
2019-04-21 03:09:00 +0100

    finished the UI design for the Personal Profile for users

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -87,20 +87,37 @@ class UserProfile extends Component {
   };
 
   onClickPersonal = e => {
+    e.preventDefault()
    this.setState({
      openPersonal:!this.state.openPersonal
    })
   }
 
   onClickContact = e => {
+    e.preventDefault()
     this.setState({
       openContact:!this.state.openContact
     })
   }
 
   onClickEducation = e => {
+    e.preventDefault()
     this.setState({
       openEducation:!this.state.openEducation
+    })
+  }
+
+  onClickUpload = e => {
+    e.preventDefault()
+    this.setState({
+      openUpload: !this.state.openUpload
+    })
+  }
+
+  onClickPhoto = e => {
+    e.preventDefault()
+    this.setState({
+      openPhoto: !this.state.openPhoto
     })
   }
 
@@ -134,6 +151,67 @@ class UserProfile extends Component {
       { label: 'Yes', value: 'Yes' }
     ];
 
+    const optionsLGA = [
+      { label: 'Barkin Ladi', value: 'Barkin Ladi' },
+      { label: 'Bassa', value: 'Bassa' },
+      { label: 'Bokkos', value: 'Bokkos' },
+      { label: 'Jos East', value: 'Jos East' },
+      { label: 'Jos North', value: 'Jos North' },
+      { label: 'Jos South', value: 'Jos South' },
+      { label: 'Kanam', value: 'Kanam' },
+      { label: 'Kanke', value: 'Kanke' },
+      { label: 'Langtang North', value: 'Langtang North' },
+      { label: 'Langtang South', value: 'Langtang South' },
+      { label: 'Mangu', value: 'Mangu' },
+      { label: 'Mikang', value: 'Mikang' },
+      { label: 'Pankshin', value: 'Pankshin' },
+      { label: "Qua'an Pan", value: "Qua'an Pan" },
+      { label: 'Riyom', value: 'Riyom' },
+      { label: 'Shendam', value: 'Shendam' },
+      { label: 'Wase', value: 'Wase' },
+    ];
+
+    const optionsYearofGrad = [
+      { label: '1991', value: '1991' },
+      { label: '1992', value: '1992' },
+      { label: '1993', value: '1993' },
+      { label: '1994', value: '1994' },
+      { label: '1995', value: '1995' },
+      { label: '1996', value: '1996' },
+      { label: '1997', value: '1997' },
+      { label: '1998', value: '1998' },
+      { label: '1999', value: '1999' },
+      { label: '2000', value: '2000' },
+      { label: '2001', value: '2001' },
+      { label: '2002', value: '2002' },
+      { label: '2003', value: '2003' },
+      { label: '2004', value: '2004' },
+      { label: '2005', value: '2005' },
+      { label: '2006', value: '2006' },
+      { label: '2007', value: '2007' },
+      { label: '2008', value: '2008' },
+      { label: '2009', value: '2009' },
+      { label: '2010', value: '1991' },
+      { label: '2011', value: '2011' },
+      { label: '2012', value: '2012' },
+      { label: '2013', value: '2013' },
+      { label: '2014', value: '2014' },
+      { label: '2015', value: '2015' },
+      { label: '2016', value: '2016' },
+      { label: '2017', value: '2017' },
+      { label: '2018', value: '2018' },
+      { label: '2019', value: '2019' },
+    ];
+
+    const optionsQualification = [
+      { label: 'School Cert', value: 'School Cert' },
+      { label: 'Olevel', value: 'Olevel' },
+      { label: 'ND', value: 'ND' },
+      { label: 'HND', value: 'HND' },
+      { label: 'Degree', value: 'Degree' },
+      { label: 'MSC', value: 'MSC' },
+      { label: 'PHD', value: 'PHD' }
+    ];
 
     const display = this.props.dashboard.isloading ?
     (<div>
@@ -172,11 +250,32 @@ class UserProfile extends Component {
                   </p>
 
                       <Button 
-                        onClick={this.onClick}
+                        onClick={this.onClickPhoto}
                         className="btn bg-warning mb-3 float-center"
                       >
                         Change
                       </Button>
+                      <Dialog
+                        open={this.state.openPhoto}
+                        onClose={this.handleClose}
+                        aria-labelledby="form-dialog-title"
+                      >
+                        <DialogTitle id="form-dialog-title">UPLOAD PHOTO</DialogTitle>
+                        <DialogContent>
+                          <DialogContentText>
+                            change you Profile Photo here
+                          </DialogContentText>
+                          
+                        </DialogContent>
+                        <DialogActions>
+                          <Button onClick={this.onClickPhoto} color="danger">
+                            Cancel
+                          </Button>
+                          <Button onClick={this.handleClose} color="success">
+                            Update
+                          </Button>
+                        </DialogActions>
+                      </Dialog>  
                 </div>
               </div>
             </div>
@@ -257,8 +356,8 @@ class UserProfile extends Component {
                             <div className="form-group mb-2">
                             <label htmlFor="inputPassword" className="col-sm-2 col-form-label">Disabilities</label>
                             <SelectListGroup
-                              placeholder="Employed"
-                              name="employed"
+                              placeholder="disability"
+                              name="disability"
                               value={this.state.diasbility}
                               onChange={this.onchange}
                               options={optionsDisability}
@@ -300,41 +399,72 @@ class UserProfile extends Component {
                     <strong>Email Address: </strong> {this.state.email}
                   </p>
                   <Button 
-                    onClick={this.onClick}
+                    onClick={this.onClickContact}
                     className="btn bg-white text-success primary mb-3 float-right">
                     Edit
                   </Button>   
-                  {/* <Dialog
-                        open={this.state.openPersonal}
+                      <Dialog
+                        open={this.state.openContact}
                         onClose={this.handleClose}
                         aria-labelledby="form-dialog-title"
                       >
-                        <DialogTitle id="form-dialog-title">PERSONAL DATA</DialogTitle>
+                        <DialogTitle id="form-dialog-title">CONTACT DATA</DialogTitle>
                         <DialogContent>
                           <DialogContentText>
-                            change you personal profile information here
+                            change you Contact information here
                           </DialogContentText>
-                          <TextField
-                            autoFocus
-                            margin="dense"
-                            id="name"
-                            label="gender"
-                            type="text"
-                            name="gender"
-                            fullWidth
-                            value={this.state.gender}
-                            onChange={this.onchange}
-                          />
+                          <br/>
+                          <div className="form-group mb-8">
+                            <label 
+                              htmlFor="inputPassword" 
+                              className="col-md-6 col-form-label">
+                              Address
+                            </label>
+                              <input 
+                                type="text" 
+                                className="form-control form-control-lg" 
+                                placeholder="Home Address" 
+                                name="address" 
+                                value={this.state.address} 
+                                onChange={this.onchange}
+                              />
+                          </div>
+                          <div className="form-group mb-2">
+                            <label htmlFor="inputPassword" className="col-sm-2 col-form-label">LGA</label>
+                            <SelectListGroup
+                              placeholder="LGA"
+                              name="lga"
+                              value={this.state.lga}
+                              onChange={this.onchange}
+                              options={optionsLGA}
+                            />
+                            </div>
+
+                            <div className="form-group mb-8">
+                            <label 
+                              htmlFor="inputPassword" 
+                              className="col-md-6 col-form-label">
+                              Email
+                            </label>
+                            <input 
+                              type="email" 
+                              className="form-control form-control-lg" 
+                              placeholder="Email Address" 
+                              name="email" 
+                              value={this.state.email} 
+                              onChange={this.onchange}
+                            />
+                          </div>
                         </DialogContent>
                         <DialogActions>
-                          <Button onClick={this.onClickPersonal} color="danger">
+                          <Button onClick={this.onClickContact} color="danger">
                             Cancel
                           </Button>
                           <Button onClick={this.handleClose} color="success">
                             Update
                           </Button>
                         </DialogActions>
-                      </Dialog>   */}
+                      </Dialog>  
                 </li>
               </ul>
             </div>
@@ -360,10 +490,91 @@ class UserProfile extends Component {
                     {" "}
                   </p>
                   <Button 
-                    onClick={this.onClick}
+                    onClick={this.onClickEducation}
                     className="btn bg-white text-success primary mb-3 float-right">
                     Edit
                   </Button> 
+                  <Dialog
+                        open={this.state.openEducation}
+                        onClose={this.handleClose}
+                        aria-labelledby="form-dialog-title"
+                      >
+                        <DialogTitle id="form-dialog-title">EDUCATION DATA</DialogTitle>
+                        <DialogContent>
+                          <DialogContentText>
+                            change you Education information here
+                          </DialogContentText>
+                          <br/>
+                          <div className="form-group mb-8">
+                            <label 
+                              htmlFor="inputPassword" 
+                              className="col-md-8 col-form-label">
+                              Year of Graduation
+                            </label> 
+                            <SelectListGroup
+                              placeholder="Year of Graduation"
+                              name="year_of_graduation"
+                              value={this.state.education.year_of_graduation}
+                              onChange={this.onchange}
+                              options={optionsYearofGrad}
+                            />
+                          </div>
+
+                            <div className="form-group mb-8">
+                            <label 
+                              htmlFor="inputPassword" 
+                              className="col-md-6 col-form-label">
+                              Institution
+                            </label>
+                            <input 
+                              type="text" 
+                              className="form-control form-control-lg" 
+                              placeholder="Institution" 
+                              name="institution" 
+                              value={this.state.education.institution} 
+                              onChange={this.onchange}
+                            />
+                          </div>
+                          <div className="form-group mb-8">
+                            <label 
+                              htmlFor="inputPassword" 
+                              className="col-md-6 col-form-label">
+                                Course
+                            </label>
+                            <input 
+                              type="text" 
+                              className="form-control form-control-lg" 
+                              placeholder="Course" 
+                              name="course" 
+                              value={this.state.education.course} 
+                              onChange={this.onchange}
+                            />
+                          </div>
+                          <div className="form-group mb-8">
+                            <label 
+                              htmlFor="inputPassword" 
+                              className="col-md-10 col-form-label">
+                              Highest Qualification
+                            </label> 
+                            <SelectListGroup
+                              placeholder="Highest Qualification"
+                              name="qualification"
+                              value={this.state.education.qualification}
+                              onChange={this.onchange}
+                              options={optionsQualification}
+                            />
+                          </div>
+
+                        </DialogContent>
+                        <DialogActions>
+                          <Button onClick={this.onClickEducation} color="danger">
+                            Cancel
+                          </Button>
+                          <Button onClick={this.handleClose} color="success">
+                            Update
+                          </Button>
+                        </DialogActions>
+                      </Dialog>  
                 </li>
                 <li className="list-group-item">
                   <h4>Uploads</h4>
@@ -378,10 +589,46 @@ class UserProfile extends Component {
                     <strong>Document Name: </strong> CV
                   </p>
                   <Button 
-                    onClick={this.onClick}
+                    onClick={this.onClickUpload}
                     className="btn bg-white text-success primary mb-3 float-right">
                     Edit
                   </Button> 
+                  <Dialog
+                        open={this.state.openUpload}
+                        onClose={this.handleClose}
+                        aria-labelledby="form-dialog-title"
+                      >
+                        <DialogTitle id="form-dialog-title">UPLOAD DOCUMENT</DialogTitle>
+                        <DialogContent>
+                          <DialogContentText>
+                            change you Uploaded Document here
+                          </DialogContentText>
+                          <br/>
+                            <div className="form-group mb-8">
+                            <label 
+                              htmlFor="inputPassword" 
+                              className="col-md-6 col-form-label">
+                              Resume
+                            </label>
+                            <input 
+                              type="file" 
+                              className="form-control form-control-lg" 
+                              placeholder="resume" 
+                              name="resume" 
+                              // value={this.state.email} 
+                              // onChange={this.onchange}
+                            />
+                          </div>
+                        </DialogContent>
+                        <DialogActions>
+                          <Button onClick={this.onClickUpload} color="danger">
+                            Cancel
+                          </Button>
+                          <Button onClick={this.handleClose} color="success">
+                            Update
+                          </Button>
+                        </DialogActions>
+                      </Dialog>  
                 </li>
               </ul>
             </div>
@@ -390,11 +637,11 @@ class UserProfile extends Component {
         </div>
       </div>
         <div className="col-md-12" style={{marginTop:"5px"}}>
-          <div className="card card-body bg-success text-white mb-3">
+          <div className="card card-body bg-white text-white mb-3">
             <div className=" text-success mb-3">
               <Button 
                 onClick={this.onClick}
-                className="btn btn-success bg-warning mb-3 text-success float-right">
+                className="btn btn-success bg-success mb-3 text-white float-right">
                 Edit
               </Button> 
             </div>
