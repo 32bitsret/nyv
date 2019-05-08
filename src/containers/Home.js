321e42be949301545fb5d40b1547e35a091b2637
church <simonreuben23@gmail.com>
2019-05-08 05:00:13 +0100

    changed the url to include website

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -6,22 +6,8 @@ import Footer from "./Footer";
 import pagesStyle from "assets/jss/material-dashboard-pro-react/layouts/pagesStyle.jsx";
 import {Redirect } from "react-router-dom";
 
-class Pages extends React.Component {
-  componentDidMount() {
-    document.body.style.overflow = "unset";
-  }
-  render() {
-    const { classes} = this.props;
-    return (
-      <div className={classes.wrapper} ref="wrapper">
-        <Redirect  to="https://pycnewwebsite.herokuapp.com/index.html" />
-      </div>
-    );
-  }
+ 
+export default () => {
+  window.location.replace("https://pycnewwebsite.herokuapp.com/index.html")
+
 }
-
-Pages.propTypes = {
-  classes: PropTypes.object.isRequired
-};
-
-export default withStyles(pagesStyle)(Pages);
